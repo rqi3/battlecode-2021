8b9b1ce18e3f23ab153c3eea6b2a955035891ed0
frodakcin <huangsy222@gmail.com>
2021-01-08 19:57:28 -0800

    space -> tab

--- src/my_player/Point.java
+++ src/my_player/Point.java
@@ -1,7 +1,7 @@
 public class Point {
-  public int x, y;
-  public Point(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
+	public int x, y;
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 }
