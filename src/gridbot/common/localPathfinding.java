d3ecb31d4a1b0954bfed6ebea491827cfcb98044
Alan-Chen99 <51056554+Alan-Chen99@users.noreply.github.com>
2021-01-16 20:36:37 -0600

    badly programmed bot that i was going to use for pathfinding but apprently it gets muckrackers near my_player

---
+++ src/gridbot/common/localPathfinding.java
@@ -0,0 +1,39 @@
+package gridbot.common;
+
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import gridbot.RobotPlayer;
+
+public class localPathfinding {
+    static RobotController rc;
+
+
+
+    public static void pathFindTo(Displacement target) throws GameActionException
+    {//TODO: better pathfinding
+        rc = RobotPlayer.rc;
+
+        System.out.println("im trying to reach: "+target);
+        MapLocation targetLocation=Displacement.add(Sensing.currentLocation,target);
+        Displacement best_direction = Displacement.CENTER;
+        int lowestDistance= Displacement.getMaxXYDistance(Sensing.currentLocation,targetLocation);
+
+
+        for(Displacement dir: Displacement.DirectionsWithoutCenter){
+            if(rc.canMove(Displacement.displacementToDirection(dir))){
+                MapLocation newLocation=Displacement.add(Sensing.currentLocation,dir);
+
+                int newDistance = Displacement.getMaxXYDistance(newLocation, targetLocation);
+                if(newDistance <= lowestDistance){
+                    best_direction = dir;
+                    lowestDistance = newDistance;
+                }
+            }
+        }
+        if(best_direction!=Displacement.CENTER){
+            rc.move(Displacement.displacementToDirection(best_direction));
+        }
+    }
+
+}
